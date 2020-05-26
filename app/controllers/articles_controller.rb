5918cd432c6ad2fdf6dd769b780e54a0a2efeef0
Pedro Donda <phfdonda@gmail.com>
2020-05-26 18:44:29 -0300

    Add edit and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
   include ArticlesHelper
 
@@ -21,8 +23,22 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy
+    redirect_to articles_path(@articles)
+  end
 
-    redirect_to @articles
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash.notice = "Article #{@article.title} was successfully updated!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
