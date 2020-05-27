d97e35c193e99f5ca3eff7d383b2c66e352af44f
Pedro Donda <phfdonda@gmail.com>
2020-05-27 14:21:37 -0300

    Create tags index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,17 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "The tag #{@tag.name} has been destroyed."
+    redirect_to tag_path(@tag)
+  end
 end
