5f625eb17d7d9510c575414c6489eeefdc3a6e28
Pedro Donda <phfdonda@gmail.com>
2020-05-27 12:10:59 -0300

    Create comments_helper

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  include CommentsHelper
+  
+  def create
+  @comment = Comment.new(comment_params)
+  @comment.article_id = params[:article_id]
+  @comment.save
+  flash.notice = 'Your comment was saved successfully'
+  redirect_to article_path(@comment.article)
+  end
+
+end
