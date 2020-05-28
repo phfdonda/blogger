6cf50214d28ef65437fa7ef3ddc47ad85f8d5401
Pedro Donda <phfdonda@gmail.com>
2020-05-28 13:55:05 -0300

    start author authentication for article change

--- db/migrate/20200527201354_sorcery_core.rb
+++ db/migrate/20200527201354_sorcery_core.rb
@@ -4,7 +4,7 @@ class SorceryCore < ActiveRecord::Migration[6.0]
       t.string :username,         null: false
       t.string :email,            null: false
       t.string :crypted_password, null: false
-      t.string :salt ,            null: false
+      t.string :salt,            null: false
 
       t.timestamps                null: false
     end
