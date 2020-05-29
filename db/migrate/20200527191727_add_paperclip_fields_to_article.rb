b4d34c5c9451e4160f54db932a475a82a549bb00
Pedro Donda <phfdonda@gmail.com>
2020-05-29 12:20:20 -0300

    Add paperclip field to change article's params. Add restrictions to log in - still incomplete

--- db/migrate/20200527191727_add_paperclip_fields_to_article.rb
+++ db/migrate/20200527191727_add_paperclip_fields_to_article.rb
@@ -4,5 +4,6 @@ class AddPaperclipFieldsToArticle < ActiveRecord::Migration[6.0]
     add_column :articles, :image_content_type, :string
     add_column :articles, :image_file_size,    :integer
     add_column :articles, :image_updated_at,   :datetime
+    add_column :articles, :author_id,          :integer
   end
 end
